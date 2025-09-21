e1afd1c3ef5d6ba2af778b5070dac51d370e5a51
marco-vidali <marcovidali@outlook.com>
2025-09-21 14:18:04 +0200

    Set up react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { lazy } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const Signup = lazy(() => import("./pages/Signup"));
+
 function App() {
-    return <h1>Soundscape</h1>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/signup" element={<Signup />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
