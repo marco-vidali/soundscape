5ccaffcc5359fa2ceb7192d42cf9f06d86b51fd5
marco-vidali <marcovidali@outlook.com>
2025-09-25 18:06:57 +0200

    Create signup hook

---
+++ src/features/auth/useSignUp.tsx
@@ -0,0 +1,12 @@
+import { useMutation } from "@tanstack/react-query";
+import { signUp as apiSignUp } from "../../services/apiAuth";
+
+export function useSignUp() {
+    const { mutate: signUp, isPending } = useMutation({
+        mutationFn: (formData: SignUpFormData) => apiSignUp(formData),
+        onSuccess: () => console.log("A verification email was sent to you"),
+        onError: () => console.error("An error has occurred while joining"),
+    });
+
+    return { signUp, isPending };
+}
