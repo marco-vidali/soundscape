b7844ec14866eaf704549b25169a49ad1cd2dfa3
marco-vidali <marcovidali@outlook.com>
2025-09-30 16:03:58 +0200

    Add useIsLoggedIn hook to check user session status

---
+++ src/features/auth/useIsLoggedIn.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import { isLoggedIn as apiIsLoggedIn } from "../../services/apiAuth";
+
+export function useIsLoggedIn() {
+    const {
+        data: loggedIn,
+        isLoading,
+        error,
+    } = useQuery({
+        queryKey: ["isLoggedIn"],
+        queryFn: () => apiIsLoggedIn(),
+    });
+
+    if (error) console.error(error);
+
+    return { loggedIn, isLoading };
+}
