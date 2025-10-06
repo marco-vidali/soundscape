25ae28d7342d2d5c029a915b591a5ec9ec6d41b0
marco-vidali <marcovidali@outlook.com>
2025-10-06 15:47:56 +0200

    Make signup link bolder

--- src/features/auth/SignupLink.tsx
+++ src/features/auth/SignupLink.tsx
@@ -4,7 +4,13 @@ const SignupLink = () => {
     return (
         <p className="text-center">
             Don't have an account?{" "}
-            <Link to={{ pathname: "/auth/signup" }}>Sign up</Link>!
+            <Link
+                to={{ pathname: "/auth/signup" }}
+                className="font-head hover:underline"
+            >
+                Sign up
+            </Link>
+            !
         </p>
     );
 };
