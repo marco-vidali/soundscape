ab82a032a5628c84b89d6b15d1b81d0494f497e0
marco-vidali <marcovidali@outlook.com>
2025-10-06 16:09:37 +0200

    Add redirect when logged in

--- src/features/auth/useLogIn.ts
+++ src/features/auth/useLogIn.ts
@@ -1,10 +1,13 @@
 import { useMutation } from "@tanstack/react-query";
 import { logIn as apiLogIn } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
 
 export function useLogIn() {
+    const navigate = useNavigate();
+
     const { mutate: logIn, isPending } = useMutation({
         mutationFn: (formData: LoginFormData) => apiLogIn(formData),
-        onSuccess: () => console.log("Logged in successfully!"),
+        onSuccess: () => navigate("/"),
         onError: (error) => console.error(error),
     });
 
