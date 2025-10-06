5999033ca745e75859bf0d98e48f413e7c805f72
marco-vidali <marcovidali@outlook.com>
2025-10-06 18:16:04 +0200

    Better login error

--- src/features/auth/useLogIn.ts
+++ src/features/auth/useLogIn.ts
@@ -1,6 +1,7 @@
 import { useMutation } from "@tanstack/react-query";
 import { logIn as apiLogIn } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 export function useLogIn() {
     const navigate = useNavigate();
@@ -8,7 +9,7 @@ export function useLogIn() {
     const { mutate: logIn, isPending } = useMutation({
         mutationFn: (formData: LoginFormData) => apiLogIn(formData),
         onSuccess: () => navigate("/"),
-        onError: (error) => console.error(error),
+        onError: () => toast.error("Your email or password is incorrect..."),
     });
 
     return { logIn, isPending };
