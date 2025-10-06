4ac4b61a5ab5bbac81f60e11596aa27217511caa
marco-vidali <marcovidali@outlook.com>
2025-10-06 15:59:22 +0200

    Better signup card

--- src/features/auth/SignupCard.tsx
+++ src/features/auth/SignupCard.tsx
@@ -1,14 +1,18 @@
-import SignupTitle from "./SignupTitle";
 import SignupForm from "./SignupForm";
 import LoginLink from "./LoginLink";
 import { Card } from "@/components/retroui/Card";
+import SignupTitle from "./SignupTitle";
 
 const SignupCard = () => {
     return (
-        <Card className="bg-transparent border-0 shadow-none w-full md:bg-card md:border-2 md:shadow-md md:w-auto">
-            <Card.Content>
-                <div className="flex flex-col justify-center gap-8 p-8">
+        <Card className="p-8 bg-transparent border-0 shadow-none w-full md:bg-card md:border-2 md:shadow-md md:w-auto">
+            <Card.Header>
+                <Card.Title>
                     <SignupTitle />
+                </Card.Title>
+            </Card.Header>
+            <Card.Content>
+                <div className="flex flex-col gap-8">
                     <SignupForm />
                     <LoginLink />
                 </div>
