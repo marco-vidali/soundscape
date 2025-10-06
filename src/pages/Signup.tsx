cc61a71b9e526c43ef6b72d61da7420faa3be670
marco-vidali <marcovidali@outlook.com>
2025-10-06 15:43:11 +0200

    Fix full screen height for mobile

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -2,7 +2,7 @@ import SignupCard from "@/features/auth/SignupCard";
 
 const Signup = () => {
     return (
-        <div className="flex justify-center items-center h-screen">
+        <div className="flex justify-center items-center h-dvh">
             <SignupCard />
         </div>
     );
