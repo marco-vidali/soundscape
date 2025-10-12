a6b2e3d265c76aa476770adaa951083b8c7d8e1b
marco-vidali <marcovidali@outlook.com>
2025-10-12 13:43:58 +0200

    Refactor FavoriteColorForm to use Controller for form handling and improve error display in SelectWithLabel

--- src/features/color/FavoriteColorForm.tsx
+++ src/features/color/FavoriteColorForm.tsx
@@ -1,11 +1,20 @@
 import SelectWithLabel from "@/ui/molecules/SelectWithLabel";
 import SubmitButton from "@/ui/molecules/SubmitButton";
 
-import { useForm, type FieldValues, type SubmitHandler } from "react-hook-form";
+import {
+    Controller,
+    useForm,
+    type FieldValues,
+    type SubmitHandler,
+} from "react-hook-form";
 import { Select } from "@/ui/atoms/Select";
 
 const FavoriteColorForm = () => {
-    const { handleSubmit, register } = useForm();
+    const {
+        handleSubmit,
+        control,
+        formState: { errors },
+    } = useForm();
 
     const onSubmit: SubmitHandler<FieldValues> = (formData) => {
         console.log(formData);
@@ -13,21 +22,28 @@ const FavoriteColorForm = () => {
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
-            <SelectWithLabel
-                label="Your Favorite Color:"
-                trigger="Pick a color"
-                {...register("favoriteColor", {
-                    required: "Your favorite color is required",
-                })}
-            >
-                <Select.Item value="purple">Purple</Select.Item>
-                <Select.Item value="yellow">Yellow</Select.Item>
-                <Select.Item value="lime">Lime</Select.Item>
-                <Select.Item value="red">Red</Select.Item>
-                <Select.Item value="lavender">Lavender</Select.Item>
-                <Select.Item value="orange">Orange</Select.Item>
-                <Select.Item value="green">Green</Select.Item>
-            </SelectWithLabel>
+            <Controller
+                name="favoriteColor"
+                control={control}
+                rules={{ required: "Your favorite color is required" }}
+                render={({ field }) => (
+                    <SelectWithLabel
+                        label="Your Favorite Color:"
+                        trigger="Pick a color"
+                        value={field.value}
+                        onValueChange={field.onChange}
+                        errors={errors.favoriteColor}
+                    >
+                        <Select.Item value="purple">Purple</Select.Item>
+                        <Select.Item value="yellow">Yellow</Select.Item>
+                        <Select.Item value="lime">Lime</Select.Item>
+                        <Select.Item value="red">Red</Select.Item>
+                        <Select.Item value="lavender">Lavender</Select.Item>
+                        <Select.Item value="orange">Orange</Select.Item>
+                        <Select.Item value="green">Green</Select.Item>
+                    </SelectWithLabel>
+                )}
+            />
 
             <SubmitButton text="Confirm" isPending={false} />
         </form>
--- src/ui/molecules/SelectWithLabel.tsx
+++ src/ui/molecules/SelectWithLabel.tsx
@@ -1,15 +1,23 @@
 import { Select } from "../atoms/Select";
 import { Label } from "../atoms/Label";
 import type { PropsWithChildren } from "react";
+import type { FieldErrors } from "react-hook-form";
+import clsx from "clsx";
 
 interface SelectWithLabelProps extends PropsWithChildren {
     label: string;
     trigger: string;
+    value: string;
+    onValueChange: (value: string) => void;
+    errors: FieldErrors[string];
 }
 
 const SelectWithLabel = ({
     label,
     trigger,
+    value,
+    onValueChange,
+    errors,
     children,
     ...otherProps
 }: SelectWithLabelProps) => {
@@ -17,14 +25,25 @@ const SelectWithLabel = ({
         <div className="flex flex-col gap-1.5">
             <Label>{label}</Label>
 
-            <Select {...otherProps}>
-                <Select.Trigger>
+            <Select value={value} onValueChange={onValueChange} {...otherProps}>
+                <Select.Trigger
+                    className={clsx(
+                        errors &&
+                            "text-destructive border-destructive shadow-none"
+                    )}
+                >
                     <Select.Value placeholder={trigger} />
                 </Select.Trigger>
                 <Select.Content>
                     <Select.Group>{children}</Select.Group>
                 </Select.Content>
             </Select>
+
+            {errors && (
+                <p className="text-sm text-destructive">
+                    {errors.message as string}
+                </p>
+            )}
         </div>
     );
 };
